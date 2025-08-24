7cd4b63e0edf5bb5a02b273a69303a2466cbd7a0
fdzr <fzamora.reina@gmail.com>
2025-08-23 22:43:01 -0400

    using fix dev set

--- run_all.sh
+++ run_all.sh
@@ -2,7 +2,7 @@
 
 python3 optimize-prompts.py \
     --train-data train_es.csv \
-    --dev-data dev_es.csv \
+    --dev-data dev_es_subset.csv \
     --number-items-dev-set 50 \
     --number-items 20 \
     --language-dataset es \
